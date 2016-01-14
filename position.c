2bd1a11f0fb9627a008a30edef0f4ebd6e42d1d6
Kévin Hassan <kevin.hassan13400@gmail.com>
2016-01-14 19:06:49 +0100

    correction sur l'implementation de position

--- position.c
+++ position.c
@@ -3,30 +3,30 @@
 Position creerPosition(int x, int y)
 {
     /* Creer une position en x,y */
-    Position *pos = malloc(sizeof(struct Position));
-    setXPos(pos, x);
-    setYPos(pos, y);
-    return pos;
+    Position* p = malloc(sizeof(Position));
+    setXPos(p, x);
+    setYPos(p, y);
+    return p;
 }
 int getXPos(Position p)
 {
     /*Retourne la coordonnee x*/
-    return p.x;
+    return p->x;
 }
-int getYPos(Position p)
+int getYPos(Positionosition p)
 {
     /*retourne la coordonnee y*/
-    return p.y;
+    return p->y;
 }
 void setXPos(Position p,int x)
 {
     /*change la coordonnee x*/
-    p.x = x;
+    p->x = x;
 }
 void setYPos(Position p,int y)
 {
-    /*change la coordonnee x*/
-    p.y = y;
+    /*change la coordonnee y*/
+    p->y = y;
 }
 bool comparerPosition(Position p1, Position p2)
 {
@@ -35,16 +35,16 @@ bool comparerPosition(Position p1, Position p2)
 }
 bool enVuePosition(Position p1, Position p2)
 {
-    /* Retourne True si les position ont au moins une coordonnée identique*/
-    return (getXPos(p1)==getXPos(p2) && getYPos(p1)!=getYPos(p2)) || (getXPos(p1)!=getXPos(p2) && getYPos(p1)==getYPos(p2))
+    /* Retourne True si les position ont au moins une coordonnÃ©e identique*/
+    return (getXPos(p1)==getXPos(p2) && getYPos(p1)!=getYPos(p2)) || (getXPos(p1)!=getXPos(p2) && getYPos(p1)==getYPos(p2));
 }
 
 bool positionValide(Position p)
 {
     /* Retourne True si la position est dans la grille et donc valide.*/
-    return ((getXPos(p) >=0) && (getXPos(p)<= 20) && (getYPos(p) >=0) && (getYPos(p) <=20));
+    return ((getXPos(p) >=0) && (getXPos(p)<= 20)) && ((getYPos(p) >=0) && (getYPos(p) <=20));
 }
 
 /*
-    Il manquait les valeurs à inserer sur les setXPos et setYPos
+    Il manquait les valeurs Ã  inserer sur les setXPos et setYPos
 */
